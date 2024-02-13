724658bb34f7d7776549a1fb9e0ecd58dfa022b2
Tony <lonson574gmail.com>
2024-02-13 15:10:24 -0400

    creacion de rama dev

--- src/app/login/LoginInput.tsx
+++ src/app/login/LoginInput.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import { Box, Grid } from "@mui/material";
+//ICONS
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import KeyIcon from '@mui/icons-material/Key';
 
 function LoginInput() {
   return (
@@ -10,15 +12,17 @@ function LoginInput() {
       className="flex p-5 justify-center "
       sx={{ width: "100%", height: "50vh" }}
     >
-      <Grid item xs={9} className="p-5">
+      <Grid item xs={0.5}>
+        <Box className="justify-center flex">
+          <AccountBoxIcon sx={{ fontSize: "4em" }} />
+        </Box>
+      </Grid>
+      <Grid item xs={9} className="p-2">
         <Box className="w-full flex-row">
-          <Grid item xs={2}>
-            <AccountBoxIcon sx={{ fontSize: "4em" }} />
-          </Grid>
           <TextField id="standard-basic" label="Standard" className="flex" />
         </Box>
       </Grid>
-      <Grid item xs={9} className="p-5">
+      <Grid item xs={9} className="p-2">
         <TextField id="standard-basic" label="Standard" className="flex" />
       </Grid>
     </Grid>
