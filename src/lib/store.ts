debefdf5aa61767547fea7bd168135480ca5e84e
Tony <lonson574gmail.com>
2024-05-31 18:41:10 -0400

    fix npm build

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,11 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import shopingCart from "@/lib/shopingCar/shopingCart";
 import usurname from "@/lib/ModalLogin/usurname";
 import password from "@/lib/ModalLogin/password"
-const store = configureStore({
-  reducer: { shopingCart: shopingCart, usurname: usurname, password: password },
-})
 
-export type RootState = ReturnType<typeof store.getState>;
-export type Dispatch = typeof store.dispatch;
+export const makeStore = () => {
+  return configureStore({
+    reducer: { shopingCart: shopingCart, usurname: usurname, password: password },
+  })
+}
 
-export default store
+export type AppStore = ReturnType<typeof makeStore>
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+  
+
+//export type RootState = ReturnType<typeof store.getState>;
+//export type Dispatch = typeof store.dispatch;
+//export default store
